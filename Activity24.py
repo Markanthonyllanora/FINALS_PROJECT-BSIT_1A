9f0153245cee4e83a7736eebe978465fb3f5c0b2
Markanthonyllanora <llanoramarkanthony@gmail.com>
2024-12-15 22:52:25 +0800

    Update Activity24.py

--- Activity24.py
+++ Activity24.py
@@ -1,18 +1,15 @@
 
 
 def act24():
-    # Import the `act23` function
-    from Activity23 import act23  # Ensure Activity23.py contains the correct act23 function
 
-    # Get the `add` function from act23
+    from Activity23 import act23 
+
     add = act23()
 
-    # Call the `add` function with arguments
-    
 
-    # Display the docstring of act23
+
     help(act23)
 
 
-# Call the `act24` function
+
 act24()
