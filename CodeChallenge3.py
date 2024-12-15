23ae721f538b7cbded6026664156fdfb955248a4
Markanthonyllanora <llanoramarkanthony@gmail.com>
2024-12-15 20:18:48 +0800

    Update CodeChallenge3.py

---
+++ CodeChallenge3.py
@@ -0,0 +1,14 @@
+def cc3():
+    name = "Mark Anthony P. Llanora"
+    age = 18
+    birthdate = ("August 28 2006")
+    gender = "male"
+    nationality = "filipino"
+    height = 166
+    weight = 59
+
+    print(f"Hi, my name is {name} I am {age}") 
+    print(f"My birth day is {birthdate} and i am {gender}.")
+    print(f"My nationality is {nationality}. My heihght is {height} and my weight in kilograms is {weight}.")
+    
+cc3()
